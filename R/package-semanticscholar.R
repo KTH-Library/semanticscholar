8b1cbd8232748949d40bd172a94816346bd7bc33
Markus Skyttner <markussk@kth.se>
2020-10-23 18:33:42 +0200

    add docs

---
+++ R/package-semanticscholar.R
@@ -0,0 +1,9 @@
+#' Data from Semantic Scholar for use in R
+#'
+#' This package provides on-demand data access to [Semantic Scholar](https://semanticscholar.org)
+#' through a lightweight API for author and publication data.
+#'
+#' @aliases semanticscholar
+#' @name semanticscholar-package
+#' @keywords package
+NULL
