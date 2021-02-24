29fd87321ea92fe65d739c7b97c360820b479924
Markus Skyttner <markussk@kth.se>
2021-02-24 20:57:50 +0100

    fix docs

--- tests/testthat/test-zotero.R
+++ tests/testthat/test-zotero.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("assembling references for use in Zotero works", {
   ids <- c("10.1038/nrn3241", "CorpusID:37220927")
   refs <- zotero_references(ids)
   is_valid <- length(refs) == 2
